97b863fc224b02888808ef41849e4af1a89a1d11
israelvalimento <icvalimento@gmail.com>
2026-01-22 15:41:10 +0800

    Done?? need to convert other data to json??????

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { About } from './component/about/about';
 import { Experience } from './component/experience/experience';
@@ -7,10 +8,47 @@ import { Project } from './component/project/project';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, About, Experience, Techstack, Project],
+  imports: [CommonModule, RouterOutlet, About, Experience, Techstack, Project],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('israelvalimento');
+
+  showBackToTop = false;
+
+  // Listen to window scroll
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollTop =
+      window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    this.showBackToTop = scrollTop > 1520;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  isDesktop = window.innerWidth >= 1024; // lg breakpoint
+  isFlipped = false;
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isDesktop = window.innerWidth >= 1024;
+
+    // reset flip when switching to desktop
+    if (this.isDesktop) {
+      this.isFlipped = false;
+    }
+  }
+
+  toggleFlip() {
+    if (!this.isDesktop) {
+      this.isFlipped = !this.isFlipped;
+    }
+  }
 }
