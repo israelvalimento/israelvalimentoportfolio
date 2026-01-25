05a823c941ec18aac0655d38b98d9608cfff391a
israelvalimento <icvalimento@gmail.com>
2026-01-25 15:29:55 +0800

    timing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Admin } from './pages/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'pages/admin', component: Admin }];
